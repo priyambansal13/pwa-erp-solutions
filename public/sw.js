a2579ca05a29471d686805db595e8ebdd26255bc
priyambansal13 <bansalpriyam17@gmail.com>
2023-03-05 09:51:26 +0000

    updated sw.js

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,8 @@ this.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheData).then((cache) => {
       cache.addAll([
-        "static/js/bundle.js ",
+        "./",
+        // "static/js/bundle.js ",
         "/manifest.json",
         "/index.html",
         "/",
